74cf71c93191733bd3656e87d211b9f97a2ab9c7
Dilsha0812 <dilshasalamkp@gmail.com>
2024-11-02 08:00:03 +0530

    added api for deleting single data by id

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -7,7 +7,7 @@ const blogController = require('../controllers/blogController');
 router.get('/blogs',blogController.getAllblog)
 router.post('/add-blog',blogController.addBlog)
 router.get('/blog/:blog_id',blogController.findABlog)
-router.get('/deleteblog/:id',blogController.deleteBlog)
+router.delete('/deleteblog/:id',blogController.deleteBlog)
 
 
 module.exports = router;
